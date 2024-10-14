d3388f6a0d0d97940aab9f3223915625051b7ed4
satishphale <phalesatish.comp@mmcoe.edu.in>
2024-10-14 20:38:55 +0530

    added get events page

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,15 +1,42 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 
-const BASE_URL = "localhost:8080"
+const BASE_URL = "http://localhost:8080/api/v1"
+
+const EventsPage = () => {
+
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => { 
+    const getEvents = async() => {
+        try {
+            const response = await axios.get(`${BASE_URL}/events`);
+            setEvents(response.data);
+        } catch( error) {
+            throw new Error(error.message || 'Failed to fetch events');
+        }
+    };
+    getEvents();
+    }, [] );
 
 
-export const getEvents = async() => {
-    try {
-        const response = await axios.get(`${BASE_URL}/events`);
-        return response.data;
-    } catch( error) {
-        throw new Error(error.message || 'Failed to fetch events');
-    }
+return (
+    <div>
+      <h1>Events List</h1>
+      <ul>
+        {events.map((event) => (
+          <li key={event.id}>
+            <h5>{event.id}</h5>
+            <h5>{event.name}</h5>
+            <h5>{event.venue}</h5>
+            <h5>{event.performer}</h5>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
+
+export default EventsPage;
 
